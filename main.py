95c803c277ee611518c3b66f6aa560ddc9f46a39
Lua Govinda Mendes Souza <govinda.souza@mercadolivre.com>
2023-10-13 11:13:29 -0300

    up

--- main.py
+++ main.py
@@ -13,25 +13,18 @@ results = {}
 async def start_check(background_tasks: BackgroundTasks):
     check_id = str(uuid.uuid4())
     background_tasks.add_task(run_check, check_id)
-    return {"check_id": check_id}
+    return check_id
 
 async def run_check(check_id):
-    # Simulando um processo demorado
-    await asyncio.sleep(1)
     # Simulando um resultado (aprovado ou reprovado)
     results[check_id] = "approved"
 
 @app.websocket("/ws/{check_id}")
 async def websocket_endpoint(websocket: WebSocket, check_id: str):
     await websocket.accept()
-    print(f"WebSocket connection accepted for check_id: {check_id}")
-    for _ in range(10):  # Espera até 10 segundos
-        if check_id in results:
-            await websocket.send_text(results[check_id])
-            print(f"Sent result for check_id: {check_id}")
-            await asyncio.sleep(2)  # Mantém a conexão aberta por 2 segundos após enviar o resultado
-            return
-        await asyncio.sleep(1)
-    print(f"No result found for check_id: {check_id} after waiting")
+    await asyncio.sleep(5)
+    await websocket.send_text(f"{results[check_id]}")
+    await asyncio.sleep(5)
+
 
 
--- manual_test.sh
+++ manual_test.sh
@@ -2,13 +2,20 @@
 
 # Passo 1: Iniciar a Verificação
 echo "Iniciando a verificação..."
-CHECK_RESPONSE=$(curl -s -X POST "http://127.0.0.1:8000/start-check")
-CHECK_ID=$(echo $CHECK_RESPONSE | jq -r '.check_id')
+CHECK_ID=$(curl -s -X POST "http://127.0.0.1:8000/start-check")
 echo "Check ID obtido: $CHECK_ID"
 
 # Passo 2: Conectar-se ao WebSocket
 echo "Conectando ao WebSocket. Aguardando resultado..."
-RESULT=$(websocat -1 "ws://127.0.0.1:8000/ws/$CHECK_ID")
+RESULT=$(timeout 15 websocat -1 "ws://127.0.0.1:8000/ws/$CHECK_ID")
+
+# Verificar o código de saída do websocat
+EXIT_CODE=$?
+if [ $EXIT_CODE -eq 124 ]; then
+    echo "websocat timed out after 15 seconds."
+elif [ $EXIT_CODE -ne 0 ]; then
+    echo "websocat encountered an error. Exit code: $EXIT_CODE"
+fi
 
 # Passo 3: Avalie os Resultados
 echo "Resultado obtido: $RESULT"
